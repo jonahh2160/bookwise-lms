9882c7b5b8620315377cbd6a5b695249b18f6ee2
cha0sdrive <gundersonisaac992@gmail.com>
2023-10-12 17:11:54 -0500

    made Transaction class and DateManager class

---
+++ DateManager.java
@@ -0,0 +1,88 @@
+
+
+
+public class DateManager {
+    //this integer array keeps track of how many days is in each month for calculations later
+    int[] daysInMonth = {-1,31,28,31,30,31,30,31,31,30,31,30,31};
+
+    //this method will take a date string input ("month/day/year") and output the integer value for month
+    public int GetMonth(String dateString) {
+        int month = -1;
+        String monthString = "";
+        for (int i = 0; i < dateString.length(); i++) {
+            if (dateString.charAt(i) != 47) {
+                monthString += dateString.charAt(i);
+            } else {
+                break;
+            }
+        }
+        month = Integer.parseInt(monthString);
+        return(month);
+    }
+
+    //this method will take a date string ("month/day/year") and return the integer value for the day
+    public int GetDay(String dateString) {
+        int day = -1;
+        String dayString = "";
+        int passed = 0;
+        for (int i = 0; i < dateString.length(); i++) {
+            if (dateString.charAt(i) != 47) {
+                if (passed == 1) {
+                dayString += dateString.charAt(i);
+                }
+            } else {
+                if (passed == 1) {
+                    break;
+                } else {
+                    passed += 1;
+                }
+            }
+        }
+        day = Integer.parseInt(dayString);
+        return(day);
+    }
+
+    //this method will take a date string ("month/day/year") and return the integer value for the year
+    public int GetYear(String dateString) {
+        int year = -1;
+        String yearString = "";
+        int passed = 0;
+        for (int i = 0; i < dateString.length(); i++) {
+            if (dateString.charAt(i) != 47) {
+                if (passed == 2) {
+                yearString += dateString.charAt(i);
+                }
+            } else {
+                if (passed == 2) {
+                    break;
+                } else {
+                    passed += 1;
+                }
+            }
+        }
+        year = Integer.parseInt(yearString);
+        return(year);
+    }
+
+    //this method will take a date string ("month/day/year") and return one integer displaying the total amount of days (used for calculating due dates etc.)
+    public int getDateValue(String dateString) {
+        int date = 0;
+        int day = GetDay(dateString);
+        int month = GetMonth(dateString);
+        int year = GetYear(dateString);
+        if (month > 1) {date += daysInMonth[month-1];} else {date += daysInMonth[13];};
+        date += day;
+        date += year*365;
+
+        return(date);
+    }
+
+    //this method takes two date strings and will output an integer of the amount of days between the first and second date
+    public int getDateDifference(String date1, String date2) {
+        int dateValue1 = getDateValue(date1);
+        int dateValue2 = getDateValue(date2);
+        return(dateValue2-dateValue1);
+    }
+
+    
+}
--- Transaction.java
+++ Transaction.java
@@ -1,3 +1,66 @@
-public class Transaction {
-    
+public class Transaction extends DateManager {
+    Book book;
+    User user;
+    String dateBorrowed, dateReturned, dateDue;
+
+    public Transaction(User user, Book book, String dateBorrowed, String dateDue) {
+        this.user = user;
+        this.book = book;
+        this.dateBorrowed = dateBorrowed;
+        this.dateDue = dateDue;
+        this.dateReturned = "";
+    }
+
+    //-------------------------------------------Basic getter/setter methods------------------------------------------------
+    public User GetUser() {
+        return(user);
+    }
+    public void SetUser(User user) {
+        this.user = user;
+    }
+    public Book GetBook() {
+        return(book);
+    }
+    public void SetBook(Book book) {
+        this.book = book;
+    }
+    public String GetDateBorrowed() {
+        return(dateBorrowed);
+    }
+    public void SetDateBorrowed(String dateBorrowed) {
+        this.dateBorrowed = dateBorrowed;
+    }
+    public String getDateDue() {
+        return(dateDue);
+    }
+    public void setDateDue(String dateDue) {
+        this.dateDue = dateDue;
+    }
+    public String getDateReturned() {
+        return(dateReturned);
+    }
+    public void setDateReturned(String dateReturned) {
+        this.dateReturned = dateReturned;
+    }
+    //---------------------------------------------------------end of basic getter/setter methods--------------------------------------
+
+    //this method returns true or false whether the transaction is complete or not (book has been returned)
+    public boolean GetStatus() {
+        if (dateReturned != "") {
+            return(true);
+        } else {
+            return(false);
+        }
+    }
+
+    //this method officially completes the transaction, sets the dateReturned, sets the book's availability to true, and calculates overdue fees
+    public void CompleteTransaction(String dateReturned) {
+        setDateReturned(dateReturned);
+        //set book availability to true
+        int daysOverdue = getDateDifference(this.dateDue,this.dateReturned);
+        if (daysOverdue > 0) {
+            //add to user account balance (overdueFee*daysOverdue)
+        }
+    }
+
 }
