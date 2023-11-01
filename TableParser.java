28d6c31a0c8dbdcbd0f92e633a03a056fe66ad71
jhampton14 <jhampton14@murraystate.edu>
2023-11-01 10:20:18 -0500

    Back to normal

--- TableParser.java
+++ TableParser.java
@@ -1,7 +1,7 @@
 // JH 11/1
 // Temporary(?) class that handles JSON parsing. Takes the JSON and inputs it into our database
 
-// import here
+import org.json.*;
 
 public class TableParser {
     BookDatabase bookDatabase;
