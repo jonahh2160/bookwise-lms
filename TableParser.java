80f91547a1d9b30f80923df5b8c1f4944796d9e9
jhampton14 <jhampton14@murraystate.edu>
2023-11-01 09:38:59 -0500

    Creation of TableParser to handle JSONs

--- TableParser.java
+++ TableParser.java
@@ -1,11 +1,14 @@
 // JH 11/1
 // Temporary(?) class that handles JSON parsing. Takes the JSON and inputs it into our database
 
-public class TableParser extends Book {
+import org.json.*;
 
-    public TableParser(String title, String author, String publisher, String genre, long isbn, int year, int length) {
-        super(title, author, publisher, genre, isbn, year, length);
-        //TODO Auto-generated constructor stub
+public class TableParser {
+    BookDatabase bookDatabase;
+
+    // Constructor that passes a bookDatabase to here
+    public TableParser(BookDatabase bookDatabase) {
+        this.bookDatabase = bookDatabase;
     }
     
 }
