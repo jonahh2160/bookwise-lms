a463c2e92aa3cea3348810a8bcc4e6bf24a1bb0a
cha0sdrive <gundersonisaac992@gmail.com>
2023-11-10 13:15:05 -0600

    created the search page gui

---
+++ SearchPageGui.java
@@ -0,0 +1,219 @@
+// IG 11/7
+// Used for testing the GUI components
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.table.*;
+
+public class SearchPageGui {
+    private Searcher searcher;
+    private BookDatabase bookDatabase;
+    private UserDatabase userDatabase;
+    private JFrame frame;
+    private JPanel bookPanel;
+    private DefaultTableModel tableModel;
+    private JTable table;
+    private JScrollPane scrollPane;
+    private JButton bookButton, userButton, currentButton;
+    JButton[] categoryButtons;
+    final int tableX = 100;
+    final int tableY = 100;
+    final int tableWidth = 600;
+    final int tableHeight = 384;
+    final String[] bookColumnNames = {"Title","Author","Genre","ID","Available"};
+    final String[] userColumnNames = {"Name","Username","ID","Status"};
+    private String[] columnNames = bookColumnNames;
+    private int cellWidth = tableWidth/columnNames.length;
+    final int cellHeight = 20;
+    final int buttonWidth = cellWidth*2;
+
+    // Constructor that initializes the frame, panel, table model, scroll pane, table, and buttons
+    public SearchPageGui(BookDatabase bookDatabase, UserDatabase userDatabase) {
+        this.searcher = new Searcher(bookDatabase,userDatabase);
+        this.bookDatabase = bookDatabase;
+        this.userDatabase = userDatabase;
+
+        // Create the JFrame
+        frame = new JFrame("Library Management System");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(960,540);
+        frame.setLocationRelativeTo(null);
+        
+        // Create the JPanel
+        bookPanel = new JPanel();
+        bookPanel.setLayout(null);
+
+        // Creating the table
+        tableModel = new DefaultTableModel() {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return(false);
+            }
+        };
+        table = new JTable(tableModel);
+        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        table.getTableHeader().repaint();
+        // Creating the scroll pane to be able to scroll through table
+        scrollPane = new JScrollPane(table);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setBounds(tableX,tableY,tableWidth,tableHeight);
+        scrollPane.setVisible(true);
+        
+
+        // Book/User buttons
+        bookButton = new JButton("Switch to Books");
+        bookButton.setSize(cellWidth*2,cellHeight);
+        bookButton.setLocation(tableX+tableWidth-(cellWidth*2),tableY-(cellHeight));
+        userButton = new JButton("Switch to Users");
+        userButton.setSize(cellWidth*2,cellHeight);
+        userButton.setLocation(tableX+tableWidth-(cellWidth*2),tableY-(cellHeight));
+        bookButton.setVisible(false);
+
+        currentButton = userButton;
+
+    }
+
+    // This method is called to refresh the page after searching, or after switching between books and users
+    private void refreshPage() {
+        // Reset cell width based on how many columns there are
+        cellWidth = tableWidth/columnNames.length;
+        // Refresh row and column count
+        tableModel.setRowCount(bookDatabase.getSize());
+        tableModel.setColumnCount(columnNames.length);
+        // Refresh the data in the table
+        if (currentButton == userButton) {
+            getBookData(bookDatabase.getArray(),tableModel,table);
+        }
+        if (currentButton == bookButton) {
+            getUserData(userDatabase.getArray(),tableModel,table);
+        }
+        // Refresh column headers
+        for(int i = 0;i < columnNames.length;i++) {
+            table.getColumnModel().getColumn(i).setHeaderValue(columnNames[i]);
+        }
+        table.getTableHeader().repaint();
+    }
+
+    // This inputs the book data into the cells
+    private void getBookData(ArrayList<Book> database,DefaultTableModel tableModel, JTable table) {
+        // Reset row count to clear the data
+        tableModel.setRowCount(0);
+        tableModel.setRowCount(database.size());
+        // Input book data into the cells
+        for(int i = 0; i < database.size();i++) {
+            table.setValueAt(database.get(i).getTitle(),i,0);
+            table.setValueAt(database.get(i).getAuthor(),i,1);
+            table.setValueAt(database.get(i).getGenre(),i,2);
+            table.setValueAt(database.get(i).getPrimaryKey(),i,3);
+            table.setValueAt(String.valueOf(database.get(i).getAvailability()),i,4);
+        }
+    }
+
+    // This inputs the user data into the cells
+    private void getUserData(ArrayList<User> database,DefaultTableModel tableModel, JTable table) {
+        // Reset row count to clear the data
+        tableModel.setRowCount(0);
+        tableModel.setRowCount(database.size());
+        // Input user data into the cells
+        for(int i = 0; i < database.size();i++) {
+            table.setValueAt(database.get(i).getFullName(),i,0);
+            table.setValueAt(database.get(i).getUsername(),i,1);
+            table.setValueAt(database.get(i).getPrimaryKey(),i,2);
+            table.setValueAt(String.valueOf(database.get(i).getActive()),i,3);
+        }
+    }
+
+    // This method does all of the logic for the search page
+    public void searchPage() {
+        // Refresh the page
+        refreshPage();
+
+        // Search bar
+        JTextField searchField = new JTextField("");
+        searchField.setSize(tableWidth-buttonWidth,cellHeight);
+        searchField.setLocation(tableX,tableY-(cellHeight));
+        searchField.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (currentButton == userButton) {
+                    getBookData(searcher.searchBook(searchField.getText()),tableModel,table);
+                } else {
+                    getUserData(searcher.searchUser(searchField.getText()),tableModel,table);
+                }
+                return;
+            }
+        });
+        bookPanel.add(searchField);
+        
+        // Logic for clicking on a table cell
+        table.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent e) {
+                int row = table.rowAtPoint(e.getPoint());
+                frame.dispose();
+            }
+        });
+
+        // Add the scrollpane(table) to the panel
+        bookPanel.add(scrollPane);
+
+        // Book/user button logic
+        userButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                userButton.setVisible(false);
+                bookButton.setVisible(true);
+                currentButton = bookButton;
+                columnNames = userColumnNames;
+                refreshPage();
+                return;
+            }
+        });
+        bookButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                userButton.setVisible(true);
+                bookButton.setVisible(false);
+                currentButton = userButton;
+                columnNames = bookColumnNames;
+                refreshPage();
+                return;
+            }
+        });
+        bookPanel.add(userButton);
+        bookPanel.add(bookButton);
+
+        // Add this panel to the frame
+        frame.add(bookPanel);
+        frame.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        BookDatabase bookDatabase = new BookDatabase();
+        UserDatabase userDatabase = new UserDatabase();
+        SearchPageGui mainPageGui = new SearchPageGui(bookDatabase,userDatabase);
+        Book book1 = new Book("MyBook","Isaac","Fantasy",202039,2003);
+        Book book2 = new Book("Another Book","Michael","Sci-Fi",504039,2022);
+        Book book3 = new Book("Yet Another One","Jonah","Fantasy",706039,2006);
+        Book book4 = new Book("MyBook","Isaac","Fantasy",202039,2003);
+        Book book5 = new Book("Another Book","Michael","Sci-Fi",504039,2022);
+        Book book6 = new Book("Yet Another One","Jonah","Fantasy",706039,2006);
+        bookDatabase.addEntry(book1);
+        bookDatabase.addEntry(book2);
+        bookDatabase.addEntry(book3);
+        bookDatabase.addEntry(book4);
+        bookDatabase.addEntry(book5);
+        bookDatabase.addEntry(book6);
+
+        User user1 = new User("Isaac Gunderson","jpgundi","1234",0);
+        User user2 = new User("Jonah Hampton","tempest","1234",0);
+        User user3 = new User("Michael Toon","mtoon","1234",0);
+        userDatabase.addEntry(user1);
+        userDatabase.addEntry(user2);
+        userDatabase.addEntry(user3);
+
+        mainPageGui.searchPage();
+        //mainPageGui.searchUserPage(userDatabase.getArray());
+    }
+
+}
--- Searcher.java
+++ Searcher.java
@@ -7,23 +7,20 @@ public class Searcher {
     // Declaration of variables (lists/objects)
     BookDatabase bookDatabase;
     UserDatabase userDatabase;
-    TransactionDatabase transactionDatabase;
     ArrayList<Book> bookList = new ArrayList<Book>();
     ArrayList<User> userList = new ArrayList<User>();
-    ArrayList<Transaction> transactionList = new ArrayList<Transaction>();
 
     // Constructor will set the list variables to refer to our three main databases
-    public Searcher(BookDatabase bookDatabase, UserDatabase userDatabase, TransactionDatabase transactionDatabase) {
+    public Searcher(BookDatabase bookDatabase, UserDatabase userDatabase) {
         this.bookDatabase = bookDatabase;
         this.userDatabase = userDatabase;
-        this.transactionDatabase = transactionDatabase;
     }
 
     // BOOK SEARCH METHODS
 
     // Check each book to see if the information matches the search term, return an
     // ArrayList of the searched books
-    public ArrayList<Book> searchBook(int category, String term) {
+    public ArrayList<Book> searchBook(String term) {
         // Make sure the book list is empty before starting
         bookList.clear();
         // Set everything to lowercase (for comparison reasons)
@@ -31,8 +28,8 @@ public class Searcher {
         // Loop through books, if any match the search term, then add them to book list
         // and return the list of searched books
         for (int i = 0; i < bookDatabase.getSize(); i++) {
-            String termParsed = getBookSearchTerm(category, i, strSearch);
-            if (strSearch.equals(termParsed)) {
+            String strCompare = getBookSearchTerm(i, strSearch);
+            if (strCompare.contains(strSearch)) {
                 bookList.add(bookDatabase.getEntry(i));
             }
         }
@@ -41,37 +38,26 @@ public class Searcher {
 
     // This is used to put the search term and actual book term in the same format
     // for comparing
-    private String getBookSearchTerm(int category, int entry, String term) {
+    private String getBookSearchTerm(int entry, String term) {
         // 0=Title;1=Author;2=Genre;3=PrimaryKey
         String strCompare = "";
         // Get different term depending on what category is given
-        if (category == 0) {
-            strCompare = bookDatabase.getEntry(entry).getTitle().toLowerCase();
-        }
-        if (category == 1) {
-            strCompare = bookDatabase.getEntry(entry).getAuthor().toLowerCase();
-        }
-        if (category == 2) {
-            strCompare = bookDatabase.getEntry(entry).getGenre().toLowerCase();
-        }
-        if (category == 3) {
-            strCompare = bookDatabase.getEntry(entry).getPrimaryKey().toLowerCase();
-        }
-        // Make the term the same length as the search term
-        String termParsed = "";
-        for (int i = 0; i < term.length(); i++) {
-            if (i < strCompare.length()) {
-                termParsed += strCompare.charAt(i);
-            }
-        }
-        return (termParsed);
+        strCompare += bookDatabase.getEntry(entry).getTitle().toLowerCase() + " ";
+
+        strCompare += bookDatabase.getEntry(entry).getAuthor().toLowerCase() + " ";
+
+        strCompare += bookDatabase.getEntry(entry).getGenre().toLowerCase() + " ";
+
+        strCompare += bookDatabase.getEntry(entry).getPrimaryKey().toLowerCase() + " ";
+        
+        return (strCompare);
     }
 
     // USER SEARCH METHODS
 
     // Check each user to see if the information matches the search term, return an
     // ArrayList of the searched users
-    public ArrayList<User> searchUser(int category, String term) {
+    public ArrayList<User> searchUser(String term) {
         // Make sure the user list is empty before starting
         userList.clear();
         // Set everything to lowercase (for comparison reasons)
@@ -79,81 +65,23 @@ public class Searcher {
         // Loop through users and if any of them match the search term, then add it to
         // user list and return the list of searched users
         for (int i = 0; i < userDatabase.getSize(); i++) {
-            String termParsed = getUserSearchTerm(category, i, strSearch);
-            if (strSearch.equals(termParsed)) {
+            String strCompare = getUserSearchTerm(i, strSearch);
+            if (strCompare.contains(strSearch)) {
                 userList.add(userDatabase.getEntry(i));
             }
         }
         return (userList);
     }
 
-    private String getUserSearchTerm(int category, int entry, String term) {
+    private String getUserSearchTerm(int entry, String term) {
         // 0=Fullname;1=Username;2=PrimaryKey
         String strCompare = "";
         // Get different term depending on what category is given
-        if (category == 0) {
-            strCompare = userDatabase.getEntry(entry).getFullName().toLowerCase();
-        }
-        if (category == 1) {
-            strCompare = userDatabase.getEntry(entry).getUsername().toLowerCase();
-        }
-        if (category == 2) {
-            strCompare = userDatabase.getEntry(entry).getPrimaryKey().toLowerCase();
-        }
-        String termParsed = "";
-        // Make sure the term is the same length as the search term
-        for (int i = 0; i < term.length(); i++) {
-            if (i < strCompare.length()) {
-                termParsed += strCompare.charAt(i);
-            }
-        }
-        return (termParsed);
+        strCompare += userDatabase.getEntry(entry).getFullName().toLowerCase() + " ";
+        strCompare += userDatabase.getEntry(entry).getUsername().toLowerCase() + " ";
+        strCompare += userDatabase.getEntry(entry).getPrimaryKey().toLowerCase() + " ";
+
+        return (strCompare);
     }
 
-    // TRANSACTION SEARCH METHODS
-
-    // Check each transaction to see if the information matches the search term,
-    // return an ArrayList of the searched transactions
-    public ArrayList<Transaction> searchTransaction(int category, String term) {
-        // Make sure transaction list is empty before starting
-        transactionList.clear();
-        // Set everything to lowercase (for comparison reasons)
-        String strSearch = term.toLowerCase();
-        // Loop through transactions and if any of them match the search term, add it to
-        // the list and return the list of searched transactions
-        for (int i = 0; i < transactionDatabase.getSize(); i++) {
-            String termParsed = getTransactionSearchTerm(category, i, strSearch);
-            if (strSearch.equals(termParsed)) {
-                transactionList.add(transactionDatabase.getEntry(i));
-            }
-        }
-        return (transactionList);
-    }
-
-    private String getTransactionSearchTerm(int category, int entry, String term) {
-        // 0=UserUsername;1=BookTitle;2=DateDue;3=PrimaryKey
-        String strCompare = "";
-        // Get different term based on what category is given
-        // TODO: Try a switch statement here instead to optimize
-        if (category == 0) {
-            strCompare = transactionDatabase.getEntry(entry).getUser().getUsername().toLowerCase();
-        }
-        if (category == 1) {
-            strCompare = transactionDatabase.getEntry(entry).getBook().getTitle().toLowerCase();
-        }
-        if (category == 2) {
-            strCompare = transactionDatabase.getEntry(entry).getDateDue().toLowerCase();
-        }
-        if (category == 3) {
-            strCompare = transactionDatabase.getEntry(entry).getPrimaryKey().toLowerCase();
-        }
-        // Make sure the actual term and search term are the same length
-        String termParsed = "";
-        for (int i = 0; i < term.length(); i++) {
-            if (i < strCompare.length()) {
-                termParsed += strCompare.charAt(i);
-            }
-        }
-        return (termParsed);
-    }
 }
